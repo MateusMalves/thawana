5876d8bcaf1a8b0f4eb30e6334e0612d27152ae4
mateusu <alves.mate@hotmail.com>
2024-08-29 00:17:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener('resize', function() {
+    console.log('Window resized to ' + window.innerWidth + 'px wide');
+    // You can add custom JS here for specific screen sizes
+});
